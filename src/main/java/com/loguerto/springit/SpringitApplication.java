926506956dea7361c9b7358e7be89da9d2bc8f3b
loguerto <loguerto@gmail.com>
2020-05-24 18:17:19 +0200

    Initial Commit for our Spring Boot Reddit Clone!

---
+++ src/main/java/com/loguerto/springit/SpringitApplication.java
@@ -0,0 +1,13 @@
+package com.loguerto.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitApplication.class, args);
+    }
+
+}
